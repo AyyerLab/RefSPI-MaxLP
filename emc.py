1078a58f9ba1cdc48f816a5606c5f56c5b9ce52a
Kartik Ayyer <kartik.ayyer@desy.de>
2019-03-27 14:42:32 +0100

    emc: Can now do multi-GPU reconstruction using MPI

--- emc.py
+++ emc.py
@@ -8,6 +8,7 @@ import time
 
 import numpy as np
 import h5py
+from mpi4py import MPI
 import cupy as cp
 from cupyx.scipy import ndimage
 import cupyx
@@ -25,22 +26,31 @@ class Dataset():
             if self.num_pix != fptr['num_pix'][0]:
                 raise AttributeError('Number of pixels in photons file does not match')
             self.num_data = fptr['place_ones'].shape[0]
-            self.ones = cp.array([len(fptr['place_ones'][i]) for i in range(self.num_data)]).astype('i4')
+            try:
+                self.ones = cp.array(fptr['ones'][:])
+            except KeyError:
+                self.ones = cp.array([len(fptr['place_ones'][i]) for i in range(self.num_data)]).astype('i4')
             self.ones_accum = cp.roll(self.ones.cumsum(), 1)
             self.ones_accum[0] = 0
             self.place_ones = cp.array(np.hstack(fptr['place_ones'][:]))
 
-            self.multi = cp.array([len(fptr['place_multi'][i]) for i in range(self.num_data)]).astype('i4')
+            try:
+                self.multi = cp.array(fptr['multi'][:])
+            except KeyError:
+                self.multi = cp.array([len(fptr['place_multi'][i]) for i in range(self.num_data)]).astype('i4')
             self.multi_accum = cp.roll(self.multi.cumsum(), 1)
             self.multi_accum[0] = 0
             self.place_multi = cp.array(np.hstack(fptr['place_multi'][:]))
             self.count_multi = cp.array(np.hstack(fptr['count_multi'][:]))
 
-            self.mean_count = (self.place_ones.shape[0] + self.count_multi.sum()) / self.num_data
-        print('%d frames with %.3f photons/frame' % (self.num_data, self.mean_count))
+            self.mean_count = float((self.place_ones.shape[0] + self.count_multi.sum()) / self.num_data)
 
 class EMC():
     def __init__(self, config_file):
+        self.comm = MPI.COMM_WORLD
+        self.rank = self.comm.rank
+        self.num_proc = self.comm.size
+
         config = configparser.ConfigParser()
         config.read(config_file)
 
@@ -54,181 +64,134 @@ class EMC():
         self.log_file = os.path.join(os.path.dirname(config_file),
                                      config.get('emc', 'log_file', fallback='EMC.log'))
 
+        stime = time.time()
         self.dset = Dataset(self.photons_file, self.size**2)
-        self.model = cp.random.random((self.size, self.size), dtype='f8') * self.dset.mean_count / self.dset.num_pix
-        self.mweights = cp.zeros((self.size, self.size), dtype='f8')
+        etime = time.time()
+        if self.rank == 0:
+            print('%d frames with %.3f photons/frame (%f s)' % (self.dset.num_data, self.dset.mean_count, etime-stime))
+            sys.stdout.flush()
+        self.model = np.empty((self.size, self.size))
+        if self.rank == 0:
+            self.model[:] = np.random.random((self.size, self.size)) * self.dset.mean_count / self.dset.num_pix
+        self.comm.Bcast([self.model, MPI.DOUBLE], root=0)
+        self.mweights = np.zeros((self.size, self.size), dtype='f8')
 
-        self._calc_prob_ones = cp.ReductionKernel(
-            in_params='raw float64 lview, int32 po',
-            out_params='float64 prob',
-            map_expr='lview[po]',
-            reduce_expr='a + b',
-            post_map_expr='prob = a',
-            identity='0',
-            name='kernel_calc_prob_ones')
-        self._calc_prob_multi = cp.ReductionKernel(
-            in_params='raw float64 lview, int32 pm, int32 cm',
-            out_params='float64 prob',
-            map_expr='lview[pm]*cm',
-            reduce_expr='a + b',
-            post_map_expr='prob = a',
-            identity='0',
-            name='kernel_calc_prob_multi')
-
-    def run_iteration(self, iternum=None, itype='rd'):
-        if itype == 'rd':
-            self._run_iteration_rd(iternum)
-        elif itype == 'dr':
-            self._run_iteration_dr(iternum)
-        else:
-            raise ValueError('Unrecognized iteration type: %s' % itype)
-
-    def _run_iteration_rd(self, iternum=None):
-        self.prob = cp.empty((self.num_rot, self.dset.num_data), dtype='f8')
+    def run_iteration(self, iternum=None):
+        num_rot_p = self.num_rot // self.num_proc
+        if self.rank < self.num_rot % self.num_proc:
+            num_rot_p += 1
+        self.prob = cp.empty((num_rot_p, self.dset.num_data), dtype='f8')
         view = cp.empty(self.size**2, dtype='f8')
-        msum = -self.model.sum()
 
         bsize_model = int(np.ceil(self.size/32.))
         bsize_data = int(np.ceil(self.dset.num_data/32.))
-        for r in range(self.num_rot):
+        msum = float(-self.model.sum())
+        dmodel = cp.array(self.model)
+        dmweights = cp.array(self.mweights)
+
+        for i, r in enumerate(range(self.rank, self.num_rot, self.num_proc)):
             kernels._slice_gen((bsize_model,)*2, (32,)*2,
-                (self.model, r/self.num_rot*2.*np.pi,
+                (dmodel, r/self.num_rot*2.*np.pi,
                  self.size, 1, view))
             kernels._calc_prob_all((bsize_data,), (32,),
                 (view, self.dset.num_data,
                  self.dset.ones, self.dset.multi,
                  self.dset.ones_accum, self.dset.multi_accum,
                  self.dset.place_ones, self.dset.place_multi, self.dset.count_multi,
-                 msum, self.prob[r]))
+                 msum, self.prob[i]))
 
-        max_exp = self.prob.max(0)
-        rmax = self.prob.argmax(axis=0)
+        max_exp_p = self.prob.max(0).get()
+        rmax_p = (self.prob.argmax(axis=0) * self.num_proc + self.rank).astype('i4').get()
+        max_exp = np.empty_like(max_exp_p)
+        self.rmax = np.empty_like(rmax_p)
+
+        self.comm.Allreduce([max_exp_p, MPI.DOUBLE], [max_exp, MPI.DOUBLE], op=MPI.MAX)
+        rmax_p[max_exp_p != max_exp] = -1
+        self.comm.Allreduce([rmax_p, MPI.INT], [self.rmax, MPI.INT], op=MPI.MAX)
+        max_exp = cp.array(max_exp)
+
         self.prob = cp.exp(self.prob - max_exp)
-        self.prob /= self.prob.sum(0)
+        psum_p = self.prob.sum(0).get()
+        psum = np.empty_like(psum_p)
+        self.comm.Allreduce([psum_p, MPI.DOUBLE], [psum, MPI.DOUBLE], op=MPI.SUM)
+        self.prob /= cp.array(psum)
         self.prob[self.prob < P_MIN] = 0
         p_norm = self.prob.sum(1)
         h_p_norm = p_norm.get()
 
-        self.model[:] = 0
-        self.mweights[:] = 0
-        uniform = cp.ones_like(self.mweights)
-        for r in range(self.num_rot):
-            if h_p_norm[r] == 0.:
+        dmodel[:] = 0
+        dmweights[:] = 0
+        for i, r in enumerate(range(self.rank, self.num_rot, self.num_proc)):
+            if h_p_norm[i] == 0.:
                 continue
             view[:] = 0
             kernels._merge_all((bsize_data,), (32,),
-                (self.prob[r], self.dset.num_data,
+                (self.prob[i], self.dset.num_data,
                  self.dset.ones, self.dset.multi,
                  self.dset.ones_accum, self.dset.multi_accum,
                  self.dset.place_ones, self.dset.place_multi, self.dset.count_multi,
                  view))
             kernels._slice_merge((bsize_model,)*2, (32,)*2,
-                (view/p_norm[r], r/self.num_rot*2.*np.pi,
-                 self.size, self.model, self.mweights))
-            #self.model += ndimage.rotate(view.reshape((self.size,)*2)/p_norm[r], r/self.num_rot*360, reshape=False, order=1)
-            #self.mweights += ndimage.rotate(uniform, r/self.num_rot*360, reshape=False, order=1)
-        self.model[self.mweights > 0] /= self.mweights[self.mweights > 0]
-
-        if iternum is None:
-            np.save('data/model.npy', self.model.get())
-        else:
-            np.save('data/model_%.3d.npy'%iternum, self.model.get())
-            np.save('data/rmax_%.3d.npy'%iternum, rmax.get()/self.num_rot*360.)
-
-    def _run_iteration_dr(self, iternum=None):
-        self.model[self.model < 1.e-20] = 1.e-20
-        views = cp.empty((self.num_rot, self.size, self.size), dtype='f8')
-        mviews = cp.zeros((self.num_rot, self.size, self.size), dtype='f8')
-
-        stime = time.time()
-        for r in range(self.num_rot):
-            views[r] = ndimage.rotate(self.model, -360.*r/self.num_rot, reshape=False, order=1, cval=1.e-20)
-            sys.stderr.write('\rExp  \t%d/%d' % (r+1, self.num_rot))
-        etime = time.time()
-        sys.stderr.write(' (%f s)\n' % (etime-stime))
-
-        views = views.reshape(self.num_rot, -1)
-        vsums = views.sum(axis=1)
-        views = cp.log(views)
-        prob = cp.empty(self.num_rot, dtype='f8')
-        rmax = cp.empty(self.dset.num_data, dtype='i4')
-        p_norm = cp.zeros(self.num_rot, dtype='f8')
-        self.model[:] = 0
-        self.mweights[:] = 0
+                (view/p_norm[i], r/self.num_rot*2.*np.pi,
+                 self.size, dmodel, dmweights))
         
-        stime = time.time()
-        for d in range(self.dset.num_data):
-            max_exp = -1e20
-            tmin = self.dset.ones_accum[d]
-            tmax = tmin + self.dset.ones[d]
-            p_o = self.dset.place_ones[tmin:tmax]
+        self.model = dmodel.get()
+        self.mweights = dmweights.get()
+        if self.rank == 0:
+            self.comm.Reduce(MPI.IN_PLACE, [self.model, MPI.DOUBLE], root=0, op=MPI.SUM)
+            self.comm.Reduce(MPI.IN_PLACE, [self.mweights, MPI.DOUBLE], root=0, op=MPI.SUM)
+            self.model[self.mweights > 0] /= self.mweights[self.mweights > 0]
 
-            tmin = self.dset.multi_accum[d]
-            tmax = tmin + self.dset.multi[d]
-            p_m = self.dset.place_multi[tmin:tmax]
-            c_m = self.dset.count_multi[tmin:tmax]
-
-            prob[:] = -vsums
-            for r in range(self.num_rot):
-                prob[r] += self._calc_prob_ones(views[r], p_o)
-                prob[r] += self._calc_prob_multi(views[r], p_m, c_m)
-
-                if prob[r] > max_exp:
-                    max_exp = prob[r]
-                    rmax[d] = r
-
-            prob = cp.exp(prob - max_exp)
-            prob /= prob.sum()
-            prob[prob < P_MIN] = 0.
-            p_norm += prob
-
-            for r in range(self.num_rot):
-                if prob[r] > 0:
-                    cupyx.scatter_add(mviews[r].ravel(), p_o, prob[r])
-                    cupyx.scatter_add(mviews[r].ravel(), p_m, prob[r]*c_m)
-
-            sys.stderr.write('\rMax  \t%d/%d'%(d+1, self.dset.num_data))
-        etime = time.time()
-        sys.stderr.write(' (%f s)\n' % (etime-stime))
-
-        stime = time.time()
-        uniform = cp.ones_like(mviews[r])
-        for r in range(self.num_rot):
-            if p_norm[r] == 0.:
-                continue
-            self.model += ndimage.rotate(mviews[r]/p_norm[r], 360.*r/self.num_rot, reshape=False, order=1)
-            self.mweights += ndimage.rotate(uniform, 360.*r/self.num_rot, reshape=False, order=1)
-            sys.stderr.write('\rComp \t%d/%d' % (r+1, self.num_rot))
-        etime = time.time()
-        sys.stderr.write(' (%f s)\n' % (etime-stime))
-
-        self.model[self.mweights > 0] /= self.mweights[self.mweights > 0]
-        if iternum is None:
-            np.save('data/model.npy', self.model.get())
+            if iternum is None:
+                np.save('data/model.npy', self.model)
+            else:
+                np.save('data/model_%.3d.npy'%iternum, self.model)
+                np.save('data/rmax_%.3d.npy'%iternum, self.rmax/self.num_rot*360.)
         else:
-            np.save('data/model_%.3d.npy'%iternum, self.model.get())
-            np.save('data/rmax_%.3d.npy'%iternum, rmax.get()/self.num_rot*360.)
+            self.comm.Reduce([self.model, MPI.DOUBLE], None, root=0, op=MPI.SUM)
+            self.comm.Reduce([self.mweights, MPI.DOUBLE], None, root=0, op=MPI.SUM)
+        self.comm.Bcast([self.model, MPI.DOUBLE], root=0)
 
 def main():
+    import socket
     parser = argparse.ArgumentParser(description='In-plane rotation EMC')
-    parser.add_argument('num_iter', help='Number of iterations', type=int)
-    parser.add_argument('-c', '--config_file',
-                        help='Path to configuration file (default: config.ini)',
-                        default='config.ini')
-    parser.add_argument('-t', '--itype',
-                        help="Iteration type, 'dr' or 'rd'. Default = 'rd'",
-                        default='rd')
+    parser.add_argument('num_iter', type=int,
+                        help='Number of iterations')
+    parser.add_argument('-c', '--config_file', default='config.ini',
+                        help='Path to configuration file (default: config.ini)')
+    parser.add_argument('-d', '--devices', default=None,
+                        help='Path to devices file')
     args = parser.parse_args()
 
+    comm = MPI.COMM_WORLD
+    rank = comm.rank
+    num_proc = comm.size
+    if args.devices is None:
+        if num_proc == 1:
+            print('Running on default device 0')
+        else:
+            print('Require a "devices" file if using multiple processes (one number per line)')
+            sys.exit(1)
+    else:
+        with open(args.devices) as f:
+            dev = int(f.readlines()[rank].strip())
+            print('Rank %d: %s (Device %d)' % (rank, socket.gethostname(), dev))
+            sys.stdout.flush()
+            cp.cuda.Device(dev).use()
+
     recon = EMC(args.config_file)
-    print('\nIter  time     change')
+    if rank == 0:
+        print('\nIter  time     change')
+        sys.stdout.flush()
     for i in range(args.num_iter):
-        m0 = cp.copy(recon.model)
+        m0 = cp.array(recon.model)
         stime = time.time()
-        recon.run_iteration(i+1, itype=args.itype)
+        recon.run_iteration(i+1)
         etime = time.time()
-        norm = float(cp.linalg.norm(recon.model - m0))
-        print('%-6d%-.2e %e' % (i+1, etime-stime, norm))
+        if rank == 0:
+            norm = float(cp.linalg.norm(cp.array(recon.model) - m0))
+            print('%-6d%-.2e %e' % (i+1, etime-stime, norm))
+            sys.stdout.flush()
 
 if __name__ == '__main__':
     main()
