dad16555381bb7757cbb75db1755cebfbb885de8
Abhishek Mall <abhishek.mall@mpsd.mpg.de>
2020-12-03 10:34:38 +0100

    Composite Object[Protein1(Static)+Protein(Wobble)]

--- make_data.py
+++ make_data.py
@@ -15,74 +15,129 @@ class DataGenerator():
         config = configparser.ConfigParser()
         config.read(config_file)
 
-        self.size = config.getint('parameters', 'size')
-        self.num_data = config.getint('make_data', 'num_data')
-        self.fluence = config.get('make_data', 'fluence', fallback='constant')
+        self.size = config.getint('parameters', 'size')                                                        # Image Size
+        self.num_data = config.getint('make_data', 'num_data')                                                 # Number of Diffraction patterns
+        self.fluence = config.get('make_data', 'fluence', fallback='constant')                                 # Intensity for incident beam
         self.mean_count = config.getfloat('make_data', 'mean_count')
         self.bg_count = config.getfloat('make_data', 'bg_count', fallback=None)
         self.rel_scale = config.getfloat('make_data', 'rel_scale', fallback=1000.)
-        self.out_file = os.path.join(os.path.dirname(config_file),
-                                     config.get('make_data', 'out_photons_file'))
+        self.dia_params = [float(s) for s in config.get('make_data', 'dia_params').split()]                    # Mean and STD in diameter of GOLD sphere
+        self.shift_sigma = config.getfloat('make_data', 'shift_sigma')                                         # STD in sphere center : GOLD
+        self.shift_sigma2 = config.getfloat('make_data', 'shift_sigma2')                                       # STD in sphere center : Protein 2(Wobbling)
 
-        if self.fluence not in ['constant', 'gamma']:
+        self.out_file = os.path.join(os.path.dirname(config_file),
+                config.get('make_data', 'out_photons_file'))                                                   # SAVE : Diffraction Pattern
+
+        if self.fluence not in ['constant', 'gamma']:                                                          # Fluence : Constant or Gamma
             raise ValueError('make_data:fluence needs to be either constant (default) or gamma')
-        with open('kernels.cu', 'r') as f:
+
+        with open('kernels.cu', 'r') as f:                                                                     # Kernels.cu
             kernels = cp.RawModule(code=f.read())
-        self.k_slice_gen_holo = kernels.get_function('slice_gen_holo')
-        self.k_slice_gen = kernels.get_function('slice_gen')
-        self.object = cp.zeros((self.size, self.size), dtype='f8')
-        self.object_sum = 0
-        self.bgmask = cp.zeros_like(self.object)
+        self.k_slice_gen_holo = kernels.get_function('slice_gen_holo')                                         # Sphere : F_s(q,d) [Fourier]
+        self.k_slice_gen = kernels.get_function('slice_gen')                                                   # In-plane rotations
+
+        self.object1 = cp.zeros((self.size, self.size), dtype='f8')                                            # Protein 1 (Static)
+        self.object2 = cp.zeros((self.size, self.size), dtype='f8')                                            # Protein 2 (Wobble)
+
+        self.object_sum1 = 0                                                                                   # Protein 1
+        self.object_sum2 = 0                                                                                   # Protein 2
+
+        self.bgmask = cp.zeros((self.size, self.size), dtype = 'f8')
         self.bgmask_sum = 0
 
     def make_obj(self, bg=False):
-        mask = self.bgmask if bg else self.object
 
-        num_circ = 80
-        mcen = self.size // 2
-        x, y = cp.indices(self.object.shape, dtype='f8')
-        for _ in range(num_circ):
-            rad = (0.7 + 0.3*cp.random.rand(1, dtype='f8')) * self.size / 25.
+        mask1 = self.bgmask if bg else self.object1                                                            # Protein 1
+        mask2 = self.bgmask if bg else self.object2                                                            # Protein 2
+
+        # Protein 1 (Static)
+
+        num_circ1 = 80
+        mcen1 = self.size // 2
+        x1, y1 = cp.indices(self.object1.shape, dtype='f8')
+        for _ in range(num_circ1):
+            rad1 = (0.7 + 0.3*cp.random.rand(1, dtype='f8')) * self.size / 25.
             while True:
-                cen = cp.random.rand(2, dtype='f8') * self.size / 5. + mcen * 4./ 5.
-                dist = float(cp.sqrt((cen[0]-mcen)**2 + (cen[1]-mcen)**2) + rad)
-                if dist < mcen:
+                cen1 = cp.random.rand(2, dtype='f8') * self.size / 5. + mcen1 * 4./ 5.
+                dist1 = float(cp.sqrt((cen1[0]-mcen1)**2 + (cen1[1]-mcen1)**2) + rad1)
+                if dist1 < mcen1:
                     break
 
-            diskrad = cp.sqrt((x - cen[0])**2 + (y - cen[1])**2)
-            mask[diskrad <= rad] += 1. - (diskrad[diskrad <= rad] / rad)**2
+            diskrad1 = cp.sqrt((x1 - cen1[0])**2 + (y1- cen1[1])**2)
+            mask1[diskrad1 <= rad1] += 1. - (diskrad1[diskrad1 <= rad1] / rad1)**2
+
+        # Protein 2 (Wobble)
+
+        num_circ2 = 25
+        mcen2 = self.size // 2
+        x2, y2 = cp.indices(self.object2.shape, dtype='f8')
+        for _ in range(num_circ2):
+            rad2 = (0.7 + 0.3*cp.random.rand(1, dtype='f8')) * self.size / 25.
+            while True:
+                cen2 = cp.random.rand(2, dtype='f8') * self.size / 10. + mcen2 * 4./ 7.
+                dist2 = float(cp.sqrt((cen2[0]-mcen2)**2 + (cen2[1]-mcen2)**2) + rad2)
+                if dist2 < mcen2:
+                    break
+
+            diskrad2 = cp.sqrt((x2 - cen2[0])**2 + (y2- cen2[1])**2)
+            mask2[diskrad2 <= rad2] += 1. - (diskrad2[diskrad2 <= rad2] / rad2)**2
+
+        # Sun Object
+
 
         if bg:
             #mask *= self.bg_count / mask.sum()
-            self.bgmask_sum = float(mask.sum())
+            self.bgmask_sum = float(mask1.sum() + mask2.sum())
         else:
             #mask *= self.mean_count / mask.sum()
-            self.object_sum = float(mask.sum())
+            self.object_sum1 = float(mask1.sum())                                                                           # Protein 1
+            self.object_sum2 = float(mask2.sum())                                                                           # Protein 2
 
             with h5py.File(self.out_file, 'a') as fptr:
-                if 'solution' in fptr:
-                    del fptr['solution']
-                fptr['solution'] = mask.get()
-        return mask
+
+                if 'solution1' in fptr:                                                                                     # Protein 1
+                    del fptr['solution1']
+                fptr['solution1'] = mask1.get()
+
+                if 'solution2' in fptr:                                                                                     # Protein 2
+                    del fptr['solution2']
+                fptr['solution2'] = mask2.get()
+
+        return mask1, mask2
 
     def parse_obj(self, bg=False):
-        mask = self.bgmask if bg else self.object
-        dset_name = 'bg' if bg else 'solution'
+        mask1 = self.bgmask if bg else self.object1                                                                         # Protein 1
+        dset_name1 = 'bg' if bg else 'solution1'
+
+        mask2 = self.bgmask if bg else self.object2
+        dset_name2 = 'bg' if bg else 'solution2'                                                                            # Protein 2
 
         with h5py.File(self.out_file, 'r') as fptr:
-            mask = cp.array(fptr[dset_name][:])
+            mask1 = cp.array(fptr[dset_name1][:])
+            mask2 = cp.array(fptr[dset_name2][:])                                                                  
 
         if bg:
-            mask *= self.bg_count / mask.sum()
-            self.bgmask_sum = float(mask.sum())
-            self.bgmask = mask
+            mask1 *= self.bg_count / mask1.sum()
+            mask2 *= self.bg_count / mask2.sum()
+
+            self.bgmask_sum = float(mask1.sum()+ mask2.sum())
+            self.bgmask = mask1 + mask2
         else:
             #mask *= self.mean_count / mask.sum()
-            self.object_sum = float(mask.sum())
-            self.object = mask
+            self.object_sum1 = float(mask1.sum())
+            self.object_sum2 = float(mask2.sum())
 
-    def make_data(self, parse=False):
-        if self.object_sum == 0.:
+            self.object1 = mask1
+            self.object2 = mask2
+
+    def make_data(self, parse=False):                                                                      # Diffraction Patterns
+        if self.object_sum1 == 0.:
+            if parse:
+                self.parse_obj()
+            else:
+                self.make_obj()
+
+        if self.object_sum2 == 0.:
             if parse:
                 self.parse_obj()
             else:
@@ -94,12 +149,24 @@ class DataGenerator():
             else:
                 self.make_obj(bg=True)
 
-        mask = cp.ones(self.object.shape, dtype='f8')
-        x, y = cp.indices(self.object.shape, dtype='f8')
-        cen = self.size // 2
-        pixrad = cp.sqrt((x - cen)**2 + (y - cen)**2)
-        mask[pixrad<4] = 0
-        mask[pixrad>=cen] = 0
+        # Protein 1
+
+        mask1 = cp.ones(self.object1.shape, dtype='f8')
+        x1, y1 = cp.indices(self.object1.shape, dtype='f8')
+        cen1 = self.size // 2
+        pixrad1 = cp.sqrt((x1 - cen1)**2 + (y1 - cen1)**2)
+        mask1[pixrad1<4] = 0
+        mask1[pixrad1>=cen1] = 0
+
+        # Protein 2
+
+        mask2 = cp.ones(self.object2.shape, dtype='f8')
+        x2, y2 = cp.indices(self.object2.shape, dtype='f8')
+        cen2 = self.size // 2
+        pixrad2 = cp.sqrt((x2 - cen2)**2 + (y2 - cen2)**2)
+        mask2[pixrad2<4] = 0
+        mask2[pixrad2>=cen2] = 0
+
 
         fptr = h5py.File(self.out_file, 'a')
         if 'ones' in fptr: del fptr['ones']
@@ -108,14 +175,17 @@ class DataGenerator():
         if 'place_multi' in fptr: del fptr['place_multi']
         if 'count_multi' in fptr: del fptr['count_multi']
         if 'num_pix' in fptr: del fptr['num_pix']
-        if 'true_shifts' in fptr: del fptr['true_shifts']
-        if 'true_diameters' in fptr: del fptr['true_diameters']
+
+        if 'true_shifts' in fptr: del fptr['true_shifts']                                       # GOLD Sphere
+        if 'true_shifts2' in fptr: del fptr['true_shifts2']                                     # Protein 2
+        if 'true_diameters' in fptr: del fptr['true_diameters']                                
         if 'true_angles' in fptr: del fptr['true_angles']
         if 'bg' in fptr: del fptr['bg']
         if 'scale' in fptr: del fptr['scale']
 
         if self.bgmask_sum > 0:
             fptr['bg'] = self.bgmask.get()
+
         fptr['num_pix'] = np.array([self.size**2])
         dtype = h5py.special_dtype(vlen=np.dtype('i4'))
         place_ones = fptr.create_dataset('place_ones', (self.num_data,), dtype=dtype)
@@ -124,37 +194,87 @@ class DataGenerator():
         ones = fptr.create_dataset('ones', (self.num_data,), dtype='i4')
         multi = fptr.create_dataset('multi', (self.num_data,), dtype='i4')
 
+        # Shifts for Sphere center
+
         #shifts = np.random.random((self.num_data, 2))*6 - 3
-        shifts = np.random.randn(self.num_data, 2)*1.
+        #shifts = np.random.randn(self.num_data, 2)*1.
         #shifts = np.zeros((self.num_data, 2))
+        shifts = np.random.randn(self.num_data, 2)*self.shift_sigma                                    # GOLD
         fptr['true_shifts'] = shifts
+
+        shifts2 = np.random.randn(self.num_data, 2)*self.shift_sigma2                                  # Protein 2
+        fptr['true_shifts2'] = shifts2
+
+        # Fluence and scaling 
+
         if self.fluence == 'gamma':
             scale = np.random.gamma(2., 0.5, self.num_data)
         else:
             scale = np.ones(self.num_data, dtype='f8')
         fptr['scale'] = scale
-        diameters = np.random.randn(self.num_data)*0.5 + 7.
+
+        # Diameters of sphere : GOLD
+
+        #diameters = np.random.randn(self.num_data)*0.5 + 7.
         #diameters = np.ones(self.num_data)*7.
+        diameters = np.random.randn(self.num_data)*self.dia_params[1] + self.dia_params[0]
         fptr['true_diameters'] = diameters
+
+
         #rel_scales = diameters**3 * 1000. / 7**3
         #scale *= rel_scales/1.e3
+
+        # Orientation
         angles = np.random.random(self.num_data) * 2. * np.pi
         #angles = np.zeros(self.num_data)
         fptr['true_angles'] = angles
 
+        # Initialization
+
         view = cp.zeros(self.size**2, dtype='f8')
         rview = cp.zeros_like(view, dtype='f8')
         zmask = cp.zeros_like(view, dtype='f8')
-        model = cp.fft.fftshift(cp.fft.fftn(cp.fft.ifftshift(self.object)))
+
+        # Models
+
+        model1 = cp.fft.fftshift(cp.fft.fftn(cp.fft.ifftshift(self.object1)))                  # Protein 1 (Static)
+        model2 = cp.fft.fftshift(cp.fft.fftn(cp.fft.ifftshift(self.object2)))                  # Protein 2 (Wobble)
+
+        # Wobble
+
+        mcen2 = self.size // 2.
+        x2, y2 = cp.indices(self.object2.shape, dtype = 'f8')
+        qx2 = (x2 - mcen2) / (2 * mcen2)
+        qy2 = (y2 - mcen2) / (2 * mcen2)
+
+
         bsize_model = int(np.ceil(self.size/32.))
         stime = time.time()
+
+
+        # diffraction Patterns 
+
         for i in range(self.num_data):
+
+            # Composite Object
+
+            model = model1 + model2 * cp.exp(2* cp.pi * 1j * ((qx2 * shifts2[i,0] + qy2 * shifts2[i,1])))      
+            if i < 5:
+                real_object = cp.fft.ifftshift(cp.fft.ifftn(cp.fft.fftshift(model)))
+                real_object = abs(real_object)
+                np.save('data/wp_std2/real_object_%.3d.npy'%i, real_object)
+            
+
+            # Add Reference
+
             self.k_slice_gen_holo((bsize_model,)*2, (32,)*2,
                 (model, shifts[i,0], shifts[i,1], diameters[i], self.rel_scale, scale[i], self.size, zmask, 0, view))
-            view *= mask.ravel()
+            view *= mask1.ravel() + mask2.ravel()  
             view *= self.mean_count / view.sum()
+
             self.k_slice_gen((bsize_model,)*2, (32,)*2,
-                (view, angles[i], 1., self.size, self.bgmask, 0, rview))
+                (view, angles[i], 1., self.size, self.bgmask, 0, rview)) 
+            
             frame = cp.random.poisson(rview, dtype='i4')
             place_ones[i] = cp.where(frame == 1)[0].get()
             place_multi[i] = cp.where(frame > 1)[0].get()
