c51d53c04cad6684ff9b24dedc5ede63b8c1eff0
Abhishek Mall <abhishek.mall@mpsd.mpg.de>
2021-05-04 10:47:24 +0200

    generate data for discrete/homogeneous objects

--- make_data.py
+++ make_data.py
@@ -21,27 +21,17 @@ class DataGenerator():
         config = configparser.ConfigParser()
         config.read(config_file)
         
-        #Image size with zero padding
         self.size = config.getint('parameters', 'size')
-        #Image region including composite object
         self.sizeC = config.getint('parameters', 'sizeC')
 
         self.num_data = config.getint('make_data', 'num_data')  
         self.fluence = config.get('make_data', 'fluence', fallback='constant')
         self.mean_count = config.getfloat('make_data', 'mean_count')
         self.bg_count = config.getfloat('make_data', 'bg_count', fallback=None)
-        
-        #Relative scaling of AuNP intensity
         self.rel_scale = config.getfloat('make_data', 'rel_scale')
-        #AuNP sphere diameter range
         self.dia_params = [float(s) for s in config.get('make_data', 'dia_params').split()]
-        #STD in sphere centers of AuNP
         self.shift_sigma = config.getfloat('make_data', 'shift_sigma')
-
-        #STD in sphere centers of O2(Wobble)
         self.shift_sigma2 = config.getfloat('make_data', 'shift_sigma2')
-        
-        #Save Data
         self.out_file = os.path.join(os.path.dirname(config_file), config.get('make_data', 'out_photons_file'))
         self.output_folder =op.join(op.dirname(config_file), config.get('emc', 'output_folder'))
 
@@ -53,18 +43,14 @@ class DataGenerator():
         with open('kernels.cu', 'r') as f:
             kernels = cp.RawModule(code=f.read())
 
-        #Holography 
         self.k_slice_gen_holo = kernels.get_function('slice_gen_holo')
-        #In-plane rotations
         self.k_slice_gen = kernels.get_function('slice_gen')
        
         #O1(Static) & O2(Wobble)                                                                                                       
         self.object1 = cp.zeros((self.size, self.size), dtype='f8')
         self.object2 = cp.zeros((self.size, self.size), dtype='f8')
-        
         self.object_sum = 0
 
-        #Background
         self.bgmask = cp.zeros((self.size, self.size), dtype = 'f8')
         self.bgmask_sum = 0
 
@@ -84,7 +70,6 @@ class DataGenerator():
         for i in range(num_circ1):
             #Random
             #rad1 = (0.7 + 0.3*cp.random.rand(1, dtype = 'f8')) * self.sizeC/ 25.
-
             #Specific
             rad1 = (0.7 + 0.3*(cp.cos(2.5*i) - cp.sin(i/4))) * self.sizeC/ 20.
 
@@ -92,7 +77,6 @@ class DataGenerator():
                 #Random
                 #cen = cp.random.rand(2, dtype='f8') * self.sizeC / 5. + mcen * 4./ 5.
                 #dist = float(cp.sqrt((cen[0] - mcen)**2 + (cen[1] - mcen)**2) + rad1)
-
                 #Specific
                 cen0 = (3 * cp.sin(2*i) + 0.5 * cp.sin(i/2)) * self.sizeC / 45.  + mcen * 4./ 4.4
                 cen1 = (0.5 * cp.cos(i/2) + 3 * cp.cos(i/2)) * self.sizeC / 45.  + mcen * 4./ 4.4
@@ -102,7 +86,6 @@ class DataGenerator():
                     break
             #Random
             #diskrad = cp.sqrt((x - cen[0])**2 + (y- cen[1])**2)
-
             #Specific
             diskrad = cp.sqrt((x - cen0)**2 + (y - cen1)**2)
 
@@ -113,7 +96,6 @@ class DataGenerator():
         for i in range(num_circ2):
             #Random
             #rad2 = (0.7 + 0.3 * cp.random.rand(1, dtype = 'f8')) * self.sizeC/ 22.
-
             #Specific
             rad2 = (0.7 + 0.3 *(cp.sin(i) - cp.cos(i/2))) * self.sizeC/ 30.
 
@@ -121,7 +103,6 @@ class DataGenerator():
                 #Random
                 #cen = cp.random.rand(2, dtype='f8') * self.sizeC / 10. + mcen * 4./6.  # Size of cluster + Distance from centre
                 #dist = float(cp.sqrt((cen[0] - mcen)**2 + (cen[1] - mcen)**2) + rad2)
-
                 #Specific
                 cen0 = (cp.sin(2*i) - 3 * cp.cos(i)) * self.sizeC / 60. + mcen * 5.71 /7.
                 cen1 = (cp.cos(i) - cp.sin(i/2)) * self.sizeC / 60. + mcen * 5.71 /7.
@@ -131,7 +112,6 @@ class DataGenerator():
                     break
             #Random
             #diskrad = cp.sqrt((x - cen[0])**2 + (y- cen[1])**2)
-
             #Specific
             diskrad = cp.sqrt((x - cen0)**2 + (y - cen1)**2)
 
@@ -248,14 +228,12 @@ class DataGenerator():
         shifts2 = np.random.randn(self.num_data, 2)*self.shift_sigma2
         fptr['true_shifts2'] = shifts2
 
-        # Fluence and scaling 
         if self.fluence == 'gamma':
             scale = np.random.gamma(2., 0.5, self.num_data)
         else:
             scale = np.ones(self.num_data, dtype='f8')
         fptr['scale'] = scale
 
-        #Diameters of AuNP sphere
         diameters = np.random.randn(self.num_data)*self.dia_params[1] + self.dia_params[0]
         fptr['true_diameters'] = diameters
 
@@ -272,10 +250,8 @@ class DataGenerator():
         rview = cp.zeros_like(view, dtype='f8')
         zmask = cp.zeros_like(view, dtype='f8')
 
-        #Fourier Models
         #O1
-        model1 = cp.fft.fftshift(cp.fft.fftn(cp.fft.ifftshift(self.object1)))
-        
+        model1 = cp.fft.fftshift(cp.fft.fftn(cp.fft.ifftshift(self.object1)))   
         O1 = abs(cp.fft.ifftshift(cp.fft.ifftn(cp.fft.fftshift(model1))))
         np.save(op.join(self.output_folder,'O1.npy'), O1)
         O1_intens = abs(model1)
@@ -283,15 +259,14 @@ class DataGenerator():
         
         #O2
         model2 = cp.fft.fftshift(cp.fft.fftn(cp.fft.ifftshift(self.object2)))
-        
         O2 = abs(cp.fft.ifftshift(cp.fft.ifftn(cp.fft.fftshift(model2))))
         np.save(op.join(self.output_folder,'O2.npy'), O2)
         O2_intens = abs(model2)
         np.save(op.join(self.output_folder,'O2_intens.npy'), O2_intens)
 
         #Blurred O2 in Composite Object
-        bcomposite_object = O1.get() + ndimage.uniform_filter(O2.get(), 6)
-        np.save(op.join(self.output_folder, 'bcomposite_object'), bcomposite_object)
+        #bcomposite_object = O1.get() + ndimage.uniform_filter(O2.get(), 6)
+        #np.save(op.join(self.output_folder, 'bcomposite_object'), bcomposite_object)
 
         bsize_model = int(np.ceil(self.size/32.))
         stime = time.time()
@@ -309,9 +284,8 @@ class DataGenerator():
             #if i % 2 == 0:
             #    model =  model1 + model2 * cp.exp(2 * cp.pi * 1j * ((qx * shifts2[i,0] + qy * shifts2[i,0])))
             #else:
-            #Homogeneous (A+B)/2
             model =  model1 + (np.fliplr(model2 * cp.exp( 2 * cp.pi * 1j * ((qx * shifts2[i,0] + (-3.7) * qy * shifts2[i,0]))))  
-                                      + model2 * cp.exp( 2 * cp.pi * 1j * ((qx * shifts2[i,0] + qy * shifts2[i,0])))) / 2
+                                     + model2 * cp.exp( 2 * cp.pi * 1j * ((qx * shifts2[i,0] + qy * shifts2[i,0])))) / 2
 
             if i < 5:
                 #Save Composite Object
