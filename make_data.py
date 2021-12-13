da48b9c8547ed968d15c75ba00e8ee8d231fa1c2
Abhishek Mall <abhishek.mall@mpsd.mpg.de>
2021-12-13 12:40:12 +0100

    Fix: Angles uniformly sampled now

--- make_data.py
+++ make_data.py
@@ -314,6 +314,7 @@ class DataGenerator():
                 view *= (mask.ravel() + wobble_mask.ravel())
             else:
                 view *= mask.ravel()
+           # print(self.mean_count/view.sum())
             view *= self.mean_count / view.sum()
             self.k_slice_gen((bsize_model,)*2, (32,)*2,
                 (view, angles[i], 1., self.size, self.bgmask, 0, rview)) 
