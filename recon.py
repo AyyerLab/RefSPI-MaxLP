77be2ca0f27e1b30c13b4bb7b3f7d12495c4323a
Kartik Ayyer <kartik.ayyer@desy.de>
2022-12-05 13:44:08 +0100

    recon: Removed MPI dependency

--- recon.py
+++ recon.py
@@ -7,7 +7,6 @@ import time
 import socket
 
 import cupy as cp
-from mpi4py import MPI
 
 from emc import EMC
 
@@ -18,50 +17,34 @@ def main():
                         help='Number of iterations')
     parser.add_argument('-c', '--config_file', default='emc_config.ini',
                         help='Path to configuration file (default: emc_config.ini)')
-    parser.add_argument('-d', '--devices',
-                        help='Path to devices file')
     parser.add_argument('-s', '--streams', type=int, default=4,
                         help='Number of streams to use (default=4)')
     args = parser.parse_args()
 
-    comm = MPI.COMM_WORLD
-    rank = comm.rank
-    num_proc = comm.size
-    if args.devices is None:
-        if num_proc == 1:
-            print('Running on default device 0')
-        else:
-            print('Require a "devices" file if using multiple processes (one number per line)')
-            sys.exit(1)
-    else:
-        with open(args.devices) as f:
-            dev = int(f.readlines()[rank].strip())
-            print('Rank %d: %s (Device %d)' % (rank, socket.gethostname(), dev))
-            sys.stdout.flush()
-            cp.cuda.Device(dev).use()
+    print('Running on default device 0')
+    cp.cuda.Device(0).use()
 
     recon = EMC(args.config_file, num_streams=args.streams)
     logf = open(op.join(recon.output_folder, 'EMC.log'), 'w')
-    if rank == 0:
-        logf.write('Iter  time(s)  change\n')
-        logf.flush()
-        avgtime = 0.
-        numavg = 0
+    logf.write('Iter  time(s)  change\n')
+    logf.flush()
+    avgtime = 0.
+    numavg = 0
+
     for i in range(args.num_iter):
         m0 = cp.array(recon.model)
         stime = time.time()
         recon.run_iteration(i+1)
         etime = time.time()
         sys.stderr.write('\r%d/%d (%f s)\n'% (i+1, args.num_iter, etime-stime))
-        if rank == 0:
-            norm = float(cp.linalg.norm(cp.array(recon.model) - m0))
-            logf.write('%-6d%-.2e %e\n' % (i+1, etime-stime, norm))
-            print('Change from last iteration: ', norm)
-            logf.flush()
-            if i > 0:
-                avgtime += etime-stime
-                numavg += 1
-    if rank == 0 and numavg > 0:
+        norm = float(cp.linalg.norm(cp.array(recon.model) - m0))
+        logf.write('%-6d%-.2e %e\n' % (i+1, etime-stime, norm))
+        print('Change from last iteration: ', norm)
+        logf.flush()
+        if i > 0:
+            avgtime += etime-stime
+            numavg += 1
+    if numavg > 0:
         print('\n%.4e s/iteration on average' % (avgtime / numavg))
     logf.close()
 
